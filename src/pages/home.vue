9af33f937f087c374fea6f4a488b2ce8dfc22442
Dotzooo <projectnba77@gmail.com>
2022-12-13 15:39:50 +0800

    fix: home page rwd layout

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,11 +2,11 @@
   <section
     class="container mx-auto w-full overflow-scroll flex flex-col justify-center relative"
   >
-    <div class="">
-      <p class="text-8xl dark:text-white">Hello ,</p>
-      <h1 class="text-9xl dark:text-white">I'm Adam</h1>
+    <div class="md:mb-7 lg:md-0">
+      <p class="text-5xl md:text-8xl dark:text-white">Hello ,</p>
+      <h1 class="text-6xl md:text-9xl dark:text-white">I'm Adam</h1>
     </div>
-    <div class="flex justify-between">
+    <div class="flex justify-between pt-12 sm:pt-0 md:pt-0">
       <div>
         <p class="dark:text-white">
           Hi! I'm a Frontend develop based in Taipei Taiwan.
@@ -18,6 +18,10 @@
         <p class="dark:text-white">
           It's beautiful to have a passion and a work at the same time, don't
           you think?
+        </p>
+        <p class="dark:text-white">
+          Here is my blog, Recorded my technical study notes, pitfalls I met,
+          and notes on software development experience
         </p>
       </div>
     </div>
